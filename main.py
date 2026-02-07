803b57c818302306cf7dc24aaa44e00d751fc15e
Kotta <mathieu2008blanche@gmail.com>
2026-02-07 17:15:43 +0100

    refactor(main): deleted unneccessary comments

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-"""Point d'entrée de l'application Photo Editor."""
 from views.main_window import MainWindow
 from controllers import (
     ImageController, 
@@ -12,16 +11,14 @@ from models import (
 
 def main():
     '''
-    Start application
+    Start application.
     '''
-    # Initialisation des composants centraux
     event_bus = EventBus()
     image_state = ImageStateManager()
     icons = IconManager()
     settings = SettingsManager()
     controller = ImageController(event_bus, image_state)
     
-    # Création et lancement de la fenêtre principale
     app = MainWindow(event_bus, settings, icons)
     app.mainloop()
 
