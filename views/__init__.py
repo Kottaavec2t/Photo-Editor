924edfbfbbc711d1a84f5d3b32e56061f09d8c8c
Kotta <mathieu2008blanche@gmail.com>
2026-02-07 23:41:28 +0100

    feat(views): views is now a module

---
+++ views/__init__.py
@@ -0,0 +1,13 @@
+from .main_window import MainWindow
+from .panel_container import PanelContainer
+from .top_bar import TopBar
+from .photo_viewer import PhotoViewer
+from .workspace import Workspace
+
+__all__ = [
+    'MainWindow',
+    'PanelContainer',
+    'TopBar',
+    'PhotoViewer',
+    'Workspace',
+]
