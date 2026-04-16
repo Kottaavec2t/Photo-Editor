f899b36581d7122fde857bbdc7585d15fa5b0f17
Kottaavec2t <mathieu2008blanche@gmail.com>
2026-04-16 22:12:26 +0200

    fix(notifications_controller): correct subscription method name for yesno notifications

--- controllers/notifications_controller.py
+++ controllers/notifications_controller.py
@@ -15,7 +15,7 @@ class NotificationsController:
         self._event_bus.subscribe("info_notification", self._info_notification)
         self._event_bus.subscribe("error_notification", self._error_notification)
         self._event_bus.subscribe("warning_notification", self._warning_notification)
-        self._event_bus.subscribe("yesno_notification", self._yesno_notification)
+        self._event_bus.subscribe("yesno_notification", self._yesno_question)
 
     def _info_notification(self, data: dict = None) -> None:
         title = data.get("title", "Info")
@@ -35,7 +35,7 @@ class NotificationsController:
 
         messagebox.showwarning(title, corpse, icon=messagebox.WARNING)
 
-    def _yesno_notification(self, data: dict = None) -> None:
+    def _yesno_question(self, data: dict = None) -> None:
         title = data.get("title", "Confirmation")
         corpse = data.get("corpse", "")
         icon = data.get("icon", messagebox.YESNO)
